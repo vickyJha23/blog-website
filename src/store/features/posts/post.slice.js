5391c95ab4a1ad17ab398a8634331961f2ddc22b
vickyJha23 <vickyjha388@gmail.com>
2025-10-15 12:32:07 +0530

    post slice update

--- src/store/features/posts/post.slice.js
+++ src/store/features/posts/post.slice.js
@@ -1,14 +1,44 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { createPostThunk, getAllPostsThunk } from "./post.thunk";
+
 
 const postSlice = createSlice({
     name: "post",
     initialState: {
          posts: [],
          isLoading: false,
+         error: null
     },
     reducers: {},
     extraReducers: (builder) => {
-         builder.addCase()         
+         
+         // create post
+        builder.addCase(createPostThunk.pending, (state) => {
+              state.isLoading = true;
+         })         
+         builder.addCase(createPostThunk.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.posts = action.payload;
+         })
+         builder.addCase(createPostThunk.rejected,(state, action) => {
+                state.isLoading = false,
+                state.posts = [],
+                state.error = action.payload?.message || action.error.message 
+         })
+
+         // get all posts
+         builder.addCase(getAllPostsThunk.pending, (state) => {
+               state.isLoading = true;
+         })
+         builder.addCase(getAllPostsThunk.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.posts = action.payload;
+         })
+         builder.addCase(getAllPostsThunk.rejected, (state, action) => {
+                state.isLoading = false;
+                state.posts = [];
+                state.error = action.payload?.message || action.error.message;
+         })
 
     }
 })
